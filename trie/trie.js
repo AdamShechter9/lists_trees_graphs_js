558d5e59b25103b8fdd9d2a805a3b5bc150fa740
Adam Shechter <adam.s.develop@gmail.com>
2016-09-19 17:00:13 -0700

    Implemented Trie insert and contains. have yet to test recursive insert implementation.

--- trie/trie.js
+++ trie/trie.js
@@ -9,16 +9,17 @@
 // .children = an array of references to potential children nodes.
 //             [0] corresponds to 'a', [1] corresponds to 'b'...[25] corresponds to 'z'
 
-function TrieNodeConstructor (word) {
+function TrieNodeConstructor (wordIn) {
     if (!(this instanceof TrieNodeConstructor)) {
         return new TrieNodeConstructor(word);
     }
-    this.word = word || null;
+    this.word = wordIn || null;
     this.children = [];
 
     for (var i = 0; i < 26; i+=1) {
         this.children[i] = null;
     }
+    //console.log("adding node", this.word);
 }
 
 // TrieSetConstructor
@@ -39,12 +40,16 @@ function TrieSetConstructor () {
 
 // Trie Set Methods
 TrieSetConstructor.prototype.contains = function (searchWord) {
+    console.log("contains->",searchWord);
+    if (!searchWord) { return {err: "error bad input"}}
     var word = searchWord.toLowerCase();
     var wordNode = "", currIndex = 0, currChar;
     var runner = this.root;
 
-    while (wordNode != word) {
+    while (currIndex < word.length) {
         currChar = word.charAt(currIndex);
+        //console.log("wordNode", wordNode,"currChar",currChar, "currIndex", currIndex);
+        //console.log(runner);
         if (runner.children[this.radixTable[currChar]]) {
             runner = runner.children[this.radixTable[currChar]];
             wordNode += currChar;
@@ -53,13 +58,78 @@ TrieSetConstructor.prototype.contains = function (searchWord) {
             return false;
         }
     }
-    return (this.word != null);
+    //console.log(runner.word);
+    return (runner.word != null);
 };
 
+TrieSetConstructor.prototype.insert = function (newWord) {
+    if (!newWord || newWord === undefined) {
+        return {err: "error in input"};
+    } else if (newWord === "") {
+        return null;
+    }
+    var runner = this.root;
+    var wordNode = "",
+        word = newWord.toLowerCase(),
+        currIndex = 0,
+        currChar;
+    console.log("inserting word", word, "to trie set");
+    while (currIndex < word.length) {
+        currChar = word.charAt(currIndex);
+        //console.log("wordNode", wordNode,"currChar",currChar, "currIndex", currIndex);
+        if (!runner.children[this.radixTable[currChar]]) {
+            runner.children[this.radixTable[currChar]] = new TrieNodeConstructor();
+        }
+        runner = runner.children[this.radixTable[currChar]];
+        wordNode += currChar;
+        currIndex += 1;
+    }
+    if (!runner.word) {
+        runner.word = word;
+        return true;
+    } else {
+        return false;
+    }
+};
 
-
+TrieSetConstructor.prototype.insertRecursive = function (newWord, runnerNode, Index) {
+    if (!newWord || newWord === undefined) {
+        return {err: "error in input"};
+    } else if (newWord === "") {
+        return null;
+    }
+    var runner = runnerNode || this.root;
+    var word = newWord.toLowerCase(),
+        currIndex = Index || 0,
+        currChar = word.charAt(currIndex);
+    console.log("inserting word", word, "to trie set");
+    if (currIndex === word.length) {
+        if (!runner.word) {
+            runner.word = word;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    //console.log("currChar",currChar, "currIndex", currIndex);
+    if (!runner.children[this.radixTable[currChar]]) {
+        runner.children[this.radixTable[currChar]] = new TrieNodeConstructor();
+    }
+    runner = runner.children[this.radixTable[currChar]];
+    currIndex += 1;
+    return this.insertRecursive(word, runner, currIndex);
+};
 
 
 // Main Body - Testing
 var trie1 = new TrieSetConstructor();
-console.log(trie1.radixTable);
+trie1.insert("hello");
+trie1.insert("world");
+//console.log(trie1.contains("hell"));
+console.log(trie1.contains("hello"));
+console.log(trie1.contains("world"));
+console.log((trie1.contains("adam")));
+console.log(trie1.contains("wor"));
+console.log(trie1.contains("hel"));
+trie1.insertRecursive("ADAM");
+console.log((trie1.contains("adam")));
